bb0ee20f06c29c00d64ed9e8e034901f98718fab
Elena Sveshnikova <lenamarinkevich@mail.ru>
2023-02-10 16:03:02 +0300

    createRequesObject in work

--- src/ResultsDone/ResultsDone.jsx
+++ src/ResultsDone/ResultsDone.jsx
@@ -9,17 +9,18 @@ const ResultsDone = () => {
     const [dates, setDates] = useState([]);
     const [dateStart, setDateStart] = useState("")
     const [dateEnd, setDateEnd] = useState("")
-    
-    const handleStartDateChange =(date)=>{
+
+    const handleStartDateChange = (date) => {
         setDateStart(date)
+        console.log('назначаю dateStart', dateStart)
     }
 
-    const handleEndDateChange =(date)=>{
+    const handleEndDateChange = (date) => {
         setDateEnd(date)
     }
 
-      // посмотреть функцию по массиву дат
-      function getDatesInRange(startDate, endDate) {
+    // функция формирует массив дат
+    function getDatesInRange(startDate, endDate) {
         let endDateClass
         endDate === "" ? endDateClass = new Date(startDate) : endDateClass = new Date(endDate);
         const startDateClass = new Date(startDate);
@@ -33,25 +34,25 @@ const ResultsDone = () => {
         }
         return datesInInterval;
     }
+    console.log(getDatesInRange(dateStart, dateEnd), '-интервал из функции, а dateStart=', dateStart);
 
-    console.log(getDatesInRange(dateStart, dateEnd));
-
-    const createRequesObject = ( arrayIdInstruments) => {
-        debugger
+    // функция по нажатию на кнопку
+    const createRequesObject = (arrayIdInstruments) => {
         const dataRequest = {
             start_date: dateStart,
             end_date: dateEnd,
             site_id: arrayIdInstruments
         }
+        console.log('dateStart по кнопке', dateStart)
         console.log(dataRequest);
     }
 
-  
+
     return (
         <div className="resultsdone">
-            <IntervalDate onStartDateChange={handleStartDateChange} onEndDateChange={handleEndDateChange}/>
-            <TableResultsDone onCreateRequesObject={createRequesObject}/>
-            <Splider dates={dates}/>
+            <IntervalDate onStartDateChange={handleStartDateChange} onEndDateChange={handleEndDateChange} />
+            <TableResultsDone onCreateRequesObject={createRequesObject} />
+            <Splider dates={dates} />
         </div>
     )
 }
